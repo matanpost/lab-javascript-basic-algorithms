45939075ba3ff2a61d8b7f11304f0f233a94569c
Matan Post <matanpost@gmail.com>
2023-01-11 16:04:37 +0100

    iteration2 finished

--- index.js
+++ index.js
@@ -3,5 +3,17 @@
 
 // Iteration 2: Conditionals
 
+const driver = "matan";
+const nav = "deniz";
+const nav2 = "max";
 
+if (driver.length > nav.length){
+  console.log(`The driver has the longest name, it ${driver.length} characters`);
+}
+else if (nav.length > driver.length){
+  console.log(`It seems that the navigator has the longest name, it has ${nav.length} characters`);
+}
+else if (nav.length === driver.length){
+  console.log(`Wow, you both have equally long names, ${nav.length} characters!`);
+}
 // Iteration 3: Loops
