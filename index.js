dfb9b103d9fd0a37cbc866ed4a949aaaa3376115
Matan Post <matanpost@gmail.com>
2023-01-11 17:01:42 +0100

    iteration3 finished

--- index.js
+++ index.js
@@ -17,3 +17,32 @@ else if (nav.length === driver.length){
   console.log(`Wow, you both have equally long names, ${nav.length} characters!`);
 }
 // Iteration 3: Loops
+//3.1
+let nameUp = "";
+  
+  for (let i = 0; i < driver.length; i++){
+    const addition = driver[i].toUpperCase() + ' ';
+    nameUp += `${addition}`;
+  };
+ console.log(nameUp.trim());
+
+//3.2
+let nameBack = "";
+for (let i = nav.length - 1; i >= 0; i--){
+    const addition = nav[i];
+    nameBack += `${addition}`;
+  };
+ console.log(nameBack);
+
+//3.3
+let someone = nav2;
+switch (someone){
+  case driver:
+    console.log("The driver's name goes first.");
+    break;
+  case nav:
+    console.log("Yo, the navigator goes first definitely.");
+    break;
+  default:
+    console.log("What?! You both have the same name?");
+}
